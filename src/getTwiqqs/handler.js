c1757cad5fc07f459140b43fd54d970035bd12dc
David Borgenvik <borgenvik.david@gmail.com>
2019-04-17 07:10:12 +0200

    added dynamic setup of IAM role for dynamodb table based on stage

--- src/getTwiqqs/handler.js
+++ src/getTwiqqs/handler.js
@@ -5,7 +5,7 @@ const dynamoHelper = require('../lib/dynamoDbHelper')
 
 export const get = async (event) => {
   try {
-    const topic = event.pathParameters.topic
+    const topic = event.pathParameters && event.pathParameters.topic ? event.pathParameters.topic : '*'
     const response = await dynamoHelper.getLatestTwiqqs({ topic })
     return resp(200, JSON.stringify(response))
   } catch (error) {
