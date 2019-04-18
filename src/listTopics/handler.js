9e864144c34f8e522363cee7914c5b86e9ccab33
johannes_uhr <johannes.uhr@tiqqe.com>
2019-04-18 13:06:16 +0200

    listTopics lambda

---
+++ src/listTopics/handler.js
@@ -0,0 +1,22 @@
+'use strict'
+import { resp } from '../lib/responses'
+
+export const list = async (event) => {
+  try {
+    const response = [ // sooo hardcoded it hurts
+      {
+        name: 'random'
+      },
+      {
+        name: 'news'
+      },
+      {
+        name: 'serverless'
+      }
+    ];
+    return resp(200, JSON.stringify(response))
+  } catch (error) {
+    console.log(error)
+    return resp(500, error.message)
+  }
+}
