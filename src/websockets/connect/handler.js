bc02fc3753036837b656b1682218bfb8d74353d3
johannes_uhr <johannes.uhr@tiqqe.com>
2019-04-24 15:57:59 +0200

    first websocket handler

---
+++ src/websockets/connect/handler.js
@@ -0,0 +1,12 @@
+'use strict'
+import { resp } from '../../lib/responses'
+
+export const connectHandler = async (event) => {
+  console.log(JSON.stringify(event));
+  try {
+    return resp(200, JSON.stringify('Hello world!'));
+  } catch (error) {
+    console.log(error)
+    return resp(500, error.message)
+  }
+}
