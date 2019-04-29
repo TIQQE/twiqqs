baac0e3ae8659ac8d3a68158dc46a379528558a5
johannes_uhr <johannes.uhr@tiqqe.com>
2019-04-29 20:28:19 +0200

    getConnections only returns emails

--- src/connections/handler.js
+++ src/connections/handler.js
@@ -4,8 +4,10 @@ import { resp } from '../lib/responses'
 const dynamoHelper = require('../lib/dynamoDbHelper')
 
 export const get = async () => {
+  const connections = await dynamoHelper.getAllConnections();
+  const emails = connections.map((con) => { return con.email});
   try {
-    return resp(200, JSON.stringify(await dynamoHelper.getAllConnections()))
+    return resp(200, JSON.stringify(emails))
   } catch (error) {
     console.log(error)
     return resp(500, error.message)
